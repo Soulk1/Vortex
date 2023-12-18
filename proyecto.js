f3d53c0feed0d209edaea218529caec3afc7cbad
Soulk1 <150753281+Soulk1@users.noreply.github.com>
2023-12-18 09:21:43 -0600

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('principal').addEventListener('click', function() {
+        alert('¡Hizo clic en Principal!');
+    });
+
+    document.getElementById('contacto').addEventListener('click', function() {
+        alert('¡Hizo clic en Contacto!');
+    });
+
+    document.getElementById('html').addEventListener('click', function() {
+        alert('¡Hizo clic en HTML!');
+    });
+
+    document.getElementById('php').addEventListener('click', function() {
+        alert('¡Hizo clic en PHP!');
+    });
+
+    document.getElementById('javascript').addEventListener('click', function() {
+        alert('¡Hizo clic en Javascript!');
+    });
+});
